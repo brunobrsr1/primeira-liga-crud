5bd99a34e9488960053c52701418a1d283bb190a
Bruno Silva <bruno.ms1silva@gmail.com>
2025-11-06 18:42:27 +0000

    Initial commit

---
+++ src/main/java/com/brunobrsr/ligaportugalcrud/LigaPortugalCrudApplication.java
@@ -0,0 +1,13 @@
+package com.brunobrsr.ligaportugalcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigaPortugalCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LigaPortugalCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brunobrsr/ligaportugalcrud/LigaPortugalCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunobrsr.ligaportugalcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LigaPortugalCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
